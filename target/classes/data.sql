40fc4ebfcb645927932813d1bf2160f15c1bd390
rrahul08 <thefirstclass555@gmail.com>
2024-03-28 12:58:24 +0530

    getting tables ready

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into todo ( id , description , done , target_date , username)
+values (10001 , 'Lean devops',false , CURRENT_DATE(),'rahulr');
+
+insert into todo ( id , description , done , target_date , username)
+values (10002 , 'Lean springboot',false , CURRENT_DATE(),'rahulr');
+
+insert into todo ( id , description , done , target_date , username)
+values (10003 , 'Lean spring framework',false , CURRENT_DATE(),'rahulr');
+
+insert into todo ( id , description , done , target_date , username)
+values (10004 , 'Lean artificial intelligence',false , CURRENT_DATE(),'rahulr');
